278f1ddd9b49ff0871e9840029662bde770dae07
Jamil Atta Junior <atta.jamil@gmail.com>
2011-01-11 14:47:27 -0200

    Add code of register and forget password

--- src/avaliacao/urls.py
+++ src/avaliacao/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls.defaults import *
-
-import utilities
-
+from django.contrib.auth.views import password_reset, password_reset_done
 from avaliacao.tickets.views import index
-
 from django.contrib import admin
 
 admin.autodiscover()
@@ -20,8 +17,18 @@ urlpatterns = patterns('',
     url(r'^ticket/', include('avaliacao.tickets.urls')),
 
     # django-registration views
-    url(r'^accounts/', include('registration.backends.default.urls')),
-
+    url(r'^accounts/', include('registration.urls')),
+    
+    url(r'^accounts/password/reset/$', password_reset, {
+        'template_name': 'registration/password_reset_form.html',
+        'email_template_name': 'registration/password_reset_email.txt',
+        'post_reset_redirect': '/accounts/password/reset/done/'},
+        name='registration.password_reset'),
+        
+    url(r'^accounts/password/reset/done/$', password_reset_done, 
+        {'template_name': 'reviewapp/password_reset_done.html'},
+        name='registration.password_reset_done'),
+        
     url(r'^$', index, name="avaliacao.ticket.index"),
 
 )
